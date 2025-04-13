3a139e189a38697dccd3297560a840cffe49798f
shubhamsingh123 <shubham16cse06@gmail.com>
2025-04-13 23:16:17 +0530

    Testing string , number , number

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ fn main(){
     let str1 = String::from("Hello  , World");
     let num1 = String::from("1");
     let num2 = String::from("2");
-    println!("{} , {} , {}" , str1 , num1 , num2);
+    let num3 = String::from("3");
+    println!("{} , {} , {} , {}" , str1 , num1 , num2  , num3);
 }
